08f54f8a17922d83d54ec552802cbe8bfa6b546d
WJFil <greg.kitchen7@gmail.com>
2023-05-04 17:22:51 -0600

    add contact me form and adjust wording on home page

--- my-portfolio/src/components/pages/Contact.js
+++ my-portfolio/src/components/pages/Contact.js
@@ -1,15 +1,24 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Form from 'react-bootstrap/Form';
 
 export default function Contact() {
   return (
     <div>
       <h1 style={{textAlign: "center"}}>Contact Me</h1>
-      <p>
-        
-
-        
-        
-      </p>
+      <div className="container mt-5">
+      <Form style={{fontSize: '20px'}}>
+      <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+        <Form.Label  >Email address</Form.Label>
+        <Form.Control size="lg" type="email" placeholder="name@example.com" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+        <Form.Label>Message</Form.Label>
+        <Form.Control size="lg"  as="textarea" rows={3} />
+      </Form.Group>
+      <button style = {{borderRadius : "5px", paddingRight: "10px", paddingLeft: "10px"}}>Send Message</button>
+    </Form>
+      </div>
     </div>
   );
 }
--- my-portfolio/src/components/pages/Home.js
+++ my-portfolio/src/components/pages/Home.js
@@ -3,13 +3,9 @@ import React from 'react';
 export default function Home() {
   return (
     <div>
-      <h1>Home Page</h1>
+      <h1 style={{textAlign: "center"}}>Home Page</h1>
       <p>
-        I am a finance and accounting professional with over 20 years experience.
-        I am proficient in using T-sql to extract and analyze data from SQL Server databases.
-        I recently completed a 6 month certificate program through the University of Utah in full stack development
-        which has given me additional capability to interact with APIs and create interactive web applications
-        using a finance and accounting perspective.
+        Thank you for visiting my portfolio page.  Please use the links to learn more about me, view my projects, and/or send me a message.
       </p>
       
     </div>
